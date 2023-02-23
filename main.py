fbce306890c270517531fa6d683cb3e915688484
Ben Smerd <bsmerd@adam.com.au>
2023-02-23 13:00:39 +1030

    init commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#  go into the project tutorials bookmark and then go into the youtube build python trading bot tutorial for a basic level of it
+
+# bot that uses the api maybe from binance to make automatic trades
+
+# use the txt file as the source for all names of coins want to do
+
+# will need to create some dank algorithms to test for percentage changes etc
